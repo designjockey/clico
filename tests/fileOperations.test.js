2494f2fada6b2c13ed7a8421b198629369f16f65
Ritesh Manchanda <me@ritesh.it>
2018-09-08 21:29:11 -0400

    Adds test for getFileList

--- tests/fileOperations.test.js
+++ tests/fileOperations.test.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
-const { appendToFile } = require('../src/fileOperations');
+const { appendToFile, getFileList } = require('../src/fileOperations');
 
 jest.mock('fs');
 const mockAppendFile = error => {
   fs.appendFile = jest.fn((...args) => args[2](error));
+};
+
+const mockReadDir = error => {
+  fs.readdir = jest.fn((...args) => args[1](error, ['file1.js', 'file2.jsx']));
 };
 
 describe('src/fileOperations', () => {
@@ -32,6 +36,28 @@ describe('src/fileOperations', () => {
       expect(() => {
         appendToFile('some/file/name.js', 'File contents');
       }).toThrowError(/^some error message$/);
+    });
+  });
+
+  describe('#getFileList', () => {
+    test('returns filename list', done => {
+      mockReadDir();
+
+      getFileList('some/dir/path/')
+        .then(filenames => {
+          expect(filenames).toEqual(['file1.js', 'file2.jsx']);
+          done();
+        })
+        .catch(error => expect(error).toBeFalsy());
+    });
+
+    test('throws error', done => {
+      mockReadDir('some error message');
+
+      getFileList('some/dir/path/').catch(error => {
+        expect(error).toBe('some error message');
+        done();
+      });
     });
   });
 });
